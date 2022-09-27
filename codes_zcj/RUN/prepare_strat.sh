fe2a44dbdf5fd1aff45c80b7051fb52c84a1e5b6
youralien <ryanlouie2021@u.northwestern.edu>
2022-09-27 11:36:09 -0500

    fixed path to train.txt in prepare_strat.sh

--- codes_zcj/RUN/prepare_strat.sh
+++ codes_zcj/RUN/prepare_strat.sh
@@ -1,7 +1,7 @@
 python prepare.py \
     --config_name strat \
     --inputter_name strat \
-    --train_input_file /home/zhengchujie/dataset/ESC/prepare_data/main/train.txt \
+    --train_input_file _reformat/train.txt \
     --max_input_length 160 \
     --max_decoder_input_length 40 \
 
